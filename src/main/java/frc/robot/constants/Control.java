5b4b6846284a3b80d298189c433eb97bd7b90b07
LionRobotics-2907 <frcteam2907@gmail.com>
2024-01-25 16:53:26 -0800

    control stuff

---
+++ src/main/java/frc/robot/constants/Control.java
@@ -0,0 +1,10 @@
+package frc.robot.constants;
+
+public class Control {
+    public static final double kDrivetrainTicksToMeters = 0; //TODO calculate
+    public static final double kArmTicksToDegrees = 0; //TODO calculate
+    public static final double kShooterTicksToRotations = 0; //TODO calculate
+    public static final double kIntakeTicksToRotations = 0; //TODO calculate
+    public static final double kHatTicksToDiameter = 0; //TODO calculate
+    public static final double kHatTicksToDepth = 0; //TODO calculate
+}
