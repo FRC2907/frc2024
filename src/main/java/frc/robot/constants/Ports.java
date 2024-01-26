f7888f345599427d08f3f8146f4862d6c9c9b487
LionRobotics-2907 <frcteam2907@gmail.com>
2024-01-25 16:31:10 -0800

    build ports file

---
+++ src/main/java/frc/robot/constants/Ports.java
@@ -0,0 +1,25 @@
+package frc.robot.constants;
+
+public class Ports {
+    public static class can {
+        public static class drivetrain {
+            public static final int[] LEFTS = {};
+            public static final int[] RIGHTS = {};
+;        }
+        public static class driver_inputs {
+            public static final int DRIVER = 0;
+            public static final int OPERATOR = 1;
+        }
+        public static class arm {
+            public static final int[] LEFTS = {};
+            public static final int[] RIGHTS = {};
+        }
+        public static class shooter {
+            public static final int[] LEFTS = {};
+            public static final int[] RIGHTS = {};
+        }
+        public static class intake {
+            public static final int[] MOTORS = {};
+        }
+    }
+}
