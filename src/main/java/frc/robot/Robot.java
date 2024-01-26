b4d1f393309d843cdb2e11d5fb65a1775ebf7d7c
SithLor <84148006+SithLor@users.noreply.github.com>
2024-01-26 00:01:52 +0000

    add More to code the Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -14,6 +15,23 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 
 public class Robot extends TimedRobot {
+
+  double currentTimeStamp = 0;
+  double lastTimestamp = 0;
+  double dt = 0;
+  double matchTime = 0;
+
+  public String m_autoSelected;
+  public boolean testinit;
+
+  public static int DRIVE_L1_ID = 3;
+  public static int DRIVE_L2_ID = 4;
+  public static int DRIVE_R1_ID = 1;
+  public static int DRIVE_R2_ID = 2;
+  public static int INTAKE_ID = 5;
+  public static int SHOOTER_ID = 6;
+  public static int ARM_L_ID = 7;
+  public static int ARM_R_ID = 8;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -22,13 +40,44 @@ public class Robot extends TimedRobot {
   public void robotInit() {}
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    Timer timer = new Timer();
+    matchTime = (double)Timer.getMatchTime();
+    currentTimeStamp = (double)Timer.getFPGATimestamp();
+    dt = currentTimeStamp - lastTimestamp;
+    lastTimestamp = currentTimeStamp;
+    
+    System.gc();
+  }
 
   @Override
   public void autonomousInit() {}
-
+  
+  
+  // This Might Not Work 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    if (testinit) {
+      // drive->zero_gyro();
+      testinit = false;
+    }
+    
+    if (m_autoSelected == "Basic") {
+      //this->basic->run();
+      this.basic.run();
+    } else if (m_autoSelected == "MultiNote") {
+      //this->multinote->run();
+      this.multinote.run();
+    } else if (m_autoSelected == "SendIt") {
+      //this->sendit->run();
+      this.sendit.run()
+    }
+    
+    else {
+      //this->basic->run();
+      this.basic.run();
+    }
+  }
 
   @Override
   public void teleopInit() {}
