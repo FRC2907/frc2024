0f1f050a962f67013d9a8c023399a4b3ef5b6297
LionRobotics-2907 <frcteam2907@gmail.com>
2024-01-25 17:40:48 -0800

    init arm code

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,4 +5,24 @@ public class Arm {
     private double angle;
     private double error;
     private double lastError;
+
+    private Arm(){  
+    }
+    private static Arm instance;
+    public static Arm getInstance(){
+        if (instance == null)
+        instance = new Arm();
+        return instance;
+    }
+
+    public void onLoop(){
+        //read angle
+        //update variables
+        //calculate output
+        //update the motors
+    }
+
+    public void setSetPoint(double _setPoint){
+        this.setPoint = _setPoint;
+    }
 }
