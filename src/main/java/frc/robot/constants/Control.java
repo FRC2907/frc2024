499c62bd4c783cdd7f45dca2b923aec92310757d
Kate Reed <josephreed2600@gmail.com>
2024-01-27 13:14:21 -0800

    set up basic intake stuff

--- src/main/java/frc/robot/constants/Control.java
+++ src/main/java/frc/robot/constants/Control.java
@@ -1,10 +1,18 @@
 package frc.robot.constants;
 
+// TODO(hojus000) let's refactor these into per-subsystem classes
+
 public class Control {
-    public static final double kDrivetrainTicksToMeters = 0; //TODO calculate
-    public static final double kArmTicksToDegrees = 0; //TODO calculate
-    public static final double kShooterTicksToRotations = 0; //TODO calculate
+    public static final double kDrivetrainTickPerMeter = 0; //TODO calculate
+    public static final double kArmTickPerDegree = 0; //TODO calculate
+    public static final double kShooterTickPerRotation = 0; //TODO calculate
     public static final double kIntakeTicksToRotations = 0; //TODO calculate
     public static final double kHatTicksToDiameter = 0; //TODO calculate
     public static final double kHatTicksToDepth = 0; //TODO calculate
+
+    public static class intake {
+        public static final double ENCODER_RPM_PER_WHEEL_RPM = 0; // TODO calculate
+        public static final double kP = 1; // TODO
+        public static double kD = 1; // TODO
+    }
 }
--- src/main/java/frc/robot/util/Util.java
+++ src/main/java/frc/robot/util/Util.java
@@ -9,12 +9,24 @@ public class Util {
 		return new Timer().get() < step_start + duration;
 	}
 
-	public static CANSparkMax createSparkGroup(int[] ids) {
+	public static CANSparkMax createSparkGroup(int[] ids) { return createSparkGroup(ids, false); }
+	public static CANSparkMax createSparkGroup(int[] ids, boolean invert) {
 		CANSparkMax[] mcs = new CANSparkMax[ids.length];
 		for (int i = 0; i < ids.length; i++) {
 			mcs[i] = new CANSparkMax(ids[i], MotorType.kBrushless);
 			if (i > 0) mcs[i].follow(mcs[0]);
 		}
+		mcs[0].setInverted(invert);
 		return mcs[0];
 	}
+
+	public static double clamp(double min, double value, double max) {
+		if (max < min) // FIXME we really oughta like, catch this or something
+			System.err.println("[EE] I was asked to clamp value " + value + " between min " + min + " and max " + max);
+		if (value < min) return min;
+		if (max < value) return max;
+		return value;
+	}
+
+	public static double clamp12(double value) { return clamp(-12.0, value, 12.0); }
 }
