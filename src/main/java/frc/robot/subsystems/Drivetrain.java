61a3994bbc62876b4c85fce51ecd234474484c2d
Myles Calkins <tungstenmyl@gmail.com>
2024-01-27 11:00:47 -0800

    setup drivetrain code

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
 
-public class Drivetrain {
-    
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.util.Util;
+
+
+public class Drivetrain extends DifferentialDrive{
+    private Drivetrain(CANSparkMax left, CANSparkMax right) {
+        super(left, right);
+    }
+    private static Drivetrain instance;
+public static Drivetrain getInstance() {
+     CANSparkMax left, right;
+    if (instance == null) {
+        left = Util.createSparkGroup(frc.robot.constants.Ports.can.drivetrain.LEFTS);
+        left.setInverted(true);
+
+        right = Util.createSparkGroup(frc.robot.constants.Ports.can.drivetrain.LEFTS);
+        right.setInverted(false);
+
+        instance = new Drivetrain(left, right);
+
+    }
+    return instance;
+  
+}
+public void onLoop(double xSpeed, double zRotation) {
+    this.curvatureDrive((xSpeed) * Math.abs(xSpeed), zRotation, Math.abs(xSpeed) < 0.1);{
+
+    }
+}
 }
