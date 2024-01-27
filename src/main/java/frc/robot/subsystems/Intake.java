0dac30cd846953f6d77a3aa7b1884491f083ed1b
Kate Reed <josephreed2600@gmail.com>
2024-01-27 11:37:50 -0800

    set up intake motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,18 +1,27 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
+import frc.robot.constants.Ports;
+import frc.robot.util.Util;
+
 public class Intake {
     private double setPoint;
     private double speed;
     private double error;
     private double lastError;
 
-    private Intake(){
+    private CANSparkMax motor;
 
+    private Intake(CANSparkMax _motor){
+        this.motor = _motor;
     }
     private static Intake instance;
     public static Intake getInstance(){
-        if (instance == null)
-        instance = new Intake();
+        if (instance == null) {
+            CANSparkMax motor = Util.createSparkGroup(Ports.can.intake.MOTORS);
+            instance = new Intake(motor);
+        }
         return instance;
     }
 
