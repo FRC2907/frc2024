4ec43df052e2059bb9613d0607c689b15029d4f7
Myles Calkins <tungstenmyl@gmail.com>
2024-01-27 11:15:11 -0800

    Replicated arm code for intake and shooter PID control code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems;
 
 public class Intake {
-    
+    private double setPoint;
+    private double speed;
+    private double error;
+    private double lastError;
+
+private Intake(){
+
+}
+private static Intake instance;
+public static Intake getInstance(){
+    if (instance == null)
+    instance = new Intake();
+    return instance;
+}
+
+public void onLoop(){
+    //detect speed
+    //update variables
+    //calculate output
+    //update the motors
+}
+
+public void setSetPoint(double _setPoint){
+    this.setPoint = _setPoint;
+}
+
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems;
 
 public class Shooter {
-    
+    private double setPoint;
+    private double speed;
+    private double error;
+    private double lastError;
+
+    private Shooter(){
+
+    }
+    private static Shooter instance;
+    public static Shooter getInstance(){
+        if (instance == null)
+        instance = new Shooter();
+        return instance;
+    }
+
+    public void onLoop(){
+        //detect speed
+        //update variables
+        //calculate output
+        //update the motors
+    }
+
+    public void setSetPoint(double _setPoint){
+        this.setPoint = _setPoint;
+    }
+
 }
