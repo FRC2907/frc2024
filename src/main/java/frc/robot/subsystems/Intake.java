f8bdaf607186acfc9459f67091dd087ea0922462
Myles Calkins <tungstenmyl@gmail.com>
2024-01-27 11:21:36 -0800

    another spacing fix for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,20 +11,20 @@ public class Intake {
     }
     private static Intake instance;
     public static Intake getInstance(){
-    if (instance == null)
-    instance = new Intake();
-    return instance;
+        if (instance == null)
+        instance = new Intake();
+        return instance;
     }
 
     public void onLoop(){
-    //detect speed
-    //update variables
-    //calculate output
-    //update the motors
+        //detect speed
+        //update variables
+        //calculate output
+        //update the motors
     }
 
     public void setSetPoint(double _setPoint){
-    this.setPoint = _setPoint;
+        this.setPoint = _setPoint;
     }
 
 }
