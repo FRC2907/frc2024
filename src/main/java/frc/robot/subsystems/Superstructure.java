22d350cd298700c57e6fd9579678e77ed95c438b
LionRobotics-2907 <frcteam2907@gmail.com>
2024-02-03 14:58:12 -0800

    superstructure state stuff

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -69,9 +69,9 @@ public class Superstructure {
     public void onLoop() {
         switch (this.state) {
             case MOVING_TO_START:
-                ;
+                break;
             case START:
-                ;
+                break;
 
             case MOVING_TO_INTAKING:
                 arm.setSetPoint(Control.arm.kFloorPosition);
@@ -79,53 +79,75 @@ public class Superstructure {
                 // TODO automatically drive up to the Note
                 if (arm.reachedSetPoint())
                     this.state = RobotState.INTAKING;
-                ;
+                break;
             case INTAKING:
+                arm.setSetPoint(Control.arm.kFloorPosition);
+                intake.setSetPoint(Control.intake.kIntakingRpm);
                 if (false /* TODO we have a note */) 
                     this.state = RobotState.HOLDING_NOTE;
-                ;
+                break;
 
             case HOLDING_NOTE:
                 arm.setSetPoint(Control.arm.kHoldingPosition);
                 intake.setSetPoint(Control.intake.kOff);
+                break;
 
             case MOVING_TO_AMP:
                 arm.setSetPoint(Control.arm.kAmpPosition);
                 // TODO automatically drive up to the Amp
-                ;
+                if (arm.reachedSetPoint()){ //TODO add drivetrain reached set point
+                    this.state = RobotState.READY_TO_SCORE_AMP;
+                }
+                break;
             case READY_TO_SCORE_AMP:
-                
-                ;
+                this.state = RobotState.SCORING_AMP;
+                break;
             case SCORING_AMP:
                 shooter.setSetPoint(Control.shooter.kAmpRPM);
                 if (false/*scoring is done */)
-                    //move to neutral position
-                ;
+                    this.state = RobotState.NEUTRAL;
+                break;
 
             case MOVING_TO_SPEAKER:
                 arm.setSetPoint(Control.arm.kSpeakerPosition);
                 // TODO automatically drive up to the Speaker
-                ;
+                if (arm.reachedSetPoint()){ //TODO add drivetrain reached set point
+                    this.state = RobotState.READY_TO_SCORE_SPEAKER;
+                }
+                break;
             case READY_TO_SCORE_SPEAKER:
-                ;
+                this.state = RobotState.SCORING_SPEAKER;
+                break;
             case SCORING_SPEAKER:
                 shooter.setSetPoint(Control.shooter.kSpeakerRPM);
-                if (false/*scoring is done */)
-                    //move to neutral position
-                ;
+                if (false/*scoring is done */){
+                    this.state = RobotState.NEUTRAL;
+                }
+                break;
 
             case PREPARING_FOR_CLIMB:
                 arm.setSetPoint(Control.arm.kClimbReadyPosition);
                 // TODO automatically drive up to the Stage
-                ;
+                if (arm.reachedSetPoint()){
+                    this.state = RobotState.CLIMBING;
+                }
+                break;
             case CLIMBING:
                 arm.setSetPoint(Control.arm.kClimbClumbPosition);
-                ;
+                if (arm.reachedSetPoint()){
+                    this.state = RobotState.HUNG;
+                }
+                break;
             case HUNG:
                 arm.setSetPoint(Control.arm.kClimbClumbPosition);
-                ;
+                break;
+            case NEUTRAL:
+                arm.setSetPoint(Control.arm.kHoldingPosition);
+                intake.setSetPoint(Control.intake.kOff);
+                shooter.setSetPoint(Control.shooter.kOff);
+                break;
             default:
-                ; // do nothing i guess
+                break; 
         }
     }
 }
