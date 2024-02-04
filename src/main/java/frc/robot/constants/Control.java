e5a593e723d8b7441f4ce39d5d069e2a193e91e9
Kate Reed <josephreed2600@gmail.com>
2024-02-04 03:37:36 -0800

    set up Note tracking with simple camera feed

--- src/main/java/frc/robot/constants/Control.java
+++ src/main/java/frc/robot/constants/Control.java
@@ -43,7 +43,7 @@ public class Control {
     }
 
     public static class camera {
-        public static final int WIDTH = 640;
-        public static final int HEIGHT = 480;
+        public static final int WIDTH = 320;
+        public static final int HEIGHT = 240;
     }
 }
--- src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
+++ src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
@@ -1,7 +1,14 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
@@ -13,10 +20,18 @@ import edu.wpi.first.cscore.CvSource;
 
 public class NoteTargetingPipeline implements Runnable {
 
+    private int w, h;
     private CvSink cvSink;
     private CvSource outputStream;
-    private Mat mat;
+    private Mat mat, u_mat;
+    private List<MatOfPoint> pts;
+    private Scalar drawColor;
+    private Comparator<MatOfPoint> sizeSorter;
+
+    private Scalar orangeLow, orangeHigh;
+
     public NoteTargetingPipeline(int w, int h) {
+        this.w = w; this.h = h;
         CameraServer
             .startAutomaticCapture()
             .setResolution(w, h)
@@ -24,6 +39,19 @@ public class NoteTargetingPipeline implements Runnable {
         this.cvSink = CameraServer.getVideo();
         this.outputStream = CameraServer.putVideo("Note Targeting", w, h);
         this.mat = new Mat();
+        this.pts = new ArrayList<>();
+        this.u_mat = new Mat();
+        this.drawColor = new Scalar(180, 255, 255); // it's grayscale sorry
+        this.sizeSorter = (a, b) -> {
+            double aa = Imgproc.contourArea(a);
+            double bb = Imgproc.contourArea(b);
+            if (aa == bb) return 0;
+            if (aa > bb) return -1; // sort descending
+            return 1;
+        };
+
+        this.orangeLow = new Scalar(4, 127, 127);
+        this.orangeHigh = new Scalar(16, 255, 255);
     }
 
     @Override
@@ -34,13 +62,25 @@ public class NoteTargetingPipeline implements Runnable {
                 continue;
             }
 
-            Imgproc.rectangle(
-                mat
-                , new Point(100, 100)
-                , new Point(400, 400)
-                , new Scalar(255, 255, 255)
-                , 5
-            );
+            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2HSV);
+            Core.inRange(mat, orangeLow, orangeHigh, mat);
+
+            pts.clear();
+            Imgproc.findContours(mat, pts, u_mat, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            pts.sort(sizeSorter);
+
+            if (pts.size() > 0) {
+                Rect bound = Imgproc.boundingRect(pts.get(0));
+                //Imgproc.rectangle(mat, bound, drawColor);
+                double line_y = bound.y + bound.height / 2;
+                double line_x = bound.x + bound.width / 2;
+                Point note_ctr = new Point(line_x, line_y);
+                Point screen_ctr = new Point(this.w / 2, line_y);
+                Imgproc.line(mat, note_ctr, screen_ctr, drawColor, 3);
+
+                // TODO report off-centeredness somehow, maybe to NT
+            }
+
             outputStream.putFrame(mat);
         }
     }
