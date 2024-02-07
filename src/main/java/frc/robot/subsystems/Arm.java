ad19cf0f6d24f9af7160747d976d4d3f30a8bd0a
LionRobotics-2907 <frcteam2907@gmail.com>
2024-02-06 16:16:41 -0800

    arm telemetry

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.constants.Control;
 import frc.robot.constants.Ports;
 import frc.robot.util.Util;
@@ -9,11 +12,16 @@ import frc.robot.util.Util;
 public class Arm implements ISubsystem {
     private double setPoint;
     private CANSparkMax motor;
+    private NetworkTable NT;
+    DoublePublisher p_position, p_velocity;
 
     private Arm(CANSparkMax _motor) {
         this.motor = _motor;
         this.motor.getEncoder().setPositionConversionFactor(1 / Control.arm.TICK_PER_DEGREE);
         this.setPDGains(Control.arm.kP, Control.arm.kD);
+        this.NT = NetworkTableInstance.getDefault().getTable("arm");
+        this.p_position = this.NT.getDoubleTopic("position").publish();
+        this.p_velocity = this.NT.getDoubleTopic("velocity").publish();
     }
 
     private static Arm instance;
@@ -61,9 +69,18 @@ public class Arm implements ISubsystem {
         this.motor.getPIDController().setD(D);
     }
 
+    public double getPosition(){
+        return this.motor.getEncoder().getPosition();
+    }
+
+    public double getVelocity(){
+        return this.motor.getEncoder().getVelocity(); //TODO add velocity conversion factor
+    }
+
     @Override
     public void submitTelemetry() {
-        // TODO Auto-generated method stub
+        p_position.set(getPosition());
+        p_velocity.set(getVelocity());
     }
 
     @Override
