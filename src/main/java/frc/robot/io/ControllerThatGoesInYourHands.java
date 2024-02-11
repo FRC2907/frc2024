5e8d64cd5a763dbe9998af4cb8a99131e8015d47
Kate Reed <josephreed2600@gmail.com>
2024-02-10 21:26:08 -0800

    set up extended controller with rumblies

--- src/main/java/frc/robot/io/ControllerThatGoesInYourHands.java
+++ src/main/java/frc/robot/io/ControllerThatGoesInYourHands.java
@@ -1,10 +1,53 @@
 package frc.robot.io;
 
-public class ControllerThatGoesInYourHands {
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.ISubsystem;
+
+public class ControllerThatGoesInYourHands extends PS4Controller implements ISubsystem {
     /* TODO
-     * - getInstance but with an ID number---fancy!
-     * - rumble() with a duration!
-     * - probably extends PS4Controller
      * - integrate this into Robot and Superstructure classes
      */
+
+    private Timer rumbleTimer;
+    private double rumbleDuration;
+
+    private ControllerThatGoesInYourHands(int port) {
+        super(port);
+        this.rumbleTimer = new Timer();
+        this.rumbleDuration = 0;
+    }
+
+    private static Map<Integer, ControllerThatGoesInYourHands> instances = new HashMap<Integer, ControllerThatGoesInYourHands>();
+    public static ControllerThatGoesInYourHands getInstance(int id) {
+        if (!instances.containsKey(id))
+            instances.put(id, new ControllerThatGoesInYourHands(id));
+        return instances.get(id);
+    }
+
+    public void rumble(double duration, double power) {
+        this.rumbleTimer.reset();
+        this.rumbleDuration = duration;
+        this.setRumble(RumbleType.kBothRumble, power);
+        this.rumbleTimer.start();
+    }
+
+    public void rumble(double duration) {
+        this.rumble(duration, 1.0);
+    }
+
+    @Override
+    public void onLoop() {
+        if (this.rumbleTimer.hasElapsed(rumbleDuration))
+            this.setRumble(RumbleType.kBothRumble, 0);
+    }
+
+    @Override
+    public void submitTelemetry() {}
+
+    @Override
+    public void receiveOptions() {}
 }
