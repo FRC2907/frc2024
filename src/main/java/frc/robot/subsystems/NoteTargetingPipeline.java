dabc202f4ac0467c216f2cf56ba632d764f778e6
Kate Reed <josephreed2600@gmail.com>
2024-02-10 19:50:41 -0800

    note tracker implements subsystem :3

--- src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
+++ src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
@@ -17,11 +17,14 @@ import org.opencv.imgproc.Imgproc;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.constants.Control;
 
 // https://docs.wpilib.org/en/stable/docs/software/vision-processing/roborio/using-the-cameraserver-on-the-roborio.html
 
-public class NoteTargetingPipeline implements Runnable {
+public class NoteTargetingPipeline implements Runnable, ISubsystem {
 
     private int w, h;
     private CvSink cvSink;
@@ -31,15 +34,17 @@ public class NoteTargetingPipeline implements Runnable {
     private Scalar drawColor;
     private Comparator<MatOfPoint> sizeSorter;
     private Predicate<MatOfPoint> sizeFilter;
-
     private Scalar orangeLow, orangeHigh;
 
+    private double dx, y;
+    private DoublePublisher p_dx, p_y;
+
     public NoteTargetingPipeline(int w, int h, Scalar orangeLow, Scalar orangeHigh) {
-        this.w = w; this.h = h;
+        this.w = w;
+        this.h = h;
         CameraServer
-            .startAutomaticCapture()
-            .setResolution(w, h)
-            ;
+                .startAutomaticCapture()
+                .setResolution(w, h);
         this.cvSink = CameraServer.getVideo();
         this.outputStream = CameraServer.putVideo("Note Targeting", w, h);
         this.i_color = new Mat();
@@ -50,59 +55,85 @@ public class NoteTargetingPipeline implements Runnable {
         this.sizeSorter = (a, b) -> {
             double aa = Imgproc.contourArea(a);
             double bb = Imgproc.contourArea(b);
-            if (aa == bb) return 0;
-            if (aa > bb) return -1; // sort descending
+            if (aa == bb)
+                return 0;
+            if (aa > bb)
+                return -1; // sort descending
             return 1;
         };
         this.sizeFilter = (contour) -> {
             // stuff at the bottom should be bigger to pass the filter
             double y = Imgproc.boundingRect(contour).y;
             // true means "remove this item from consideration"
-            return Imgproc.contourArea(contour) < w*y*Control.camera.kAreaFilterFactor;
+            return Imgproc.contourArea(contour) < w * y * Control.camera.kAreaFilterFactor;
         };
 
         this.orangeLow = orangeLow;
         this.orangeHigh = orangeHigh;
+
+        this.dx = 0;
+        this.y = 0;
+        NetworkTable nt = NetworkTableInstance.getDefault().getTable("note-tracking");
+        this.p_dx = nt.getDoubleTopic("dx").publish();
+        this.p_y = nt.getDoubleTopic("y").publish();
     }
 
     @Override
     public void run() {
-        while (!Thread.interrupted()) {
-            if (this.cvSink.grabFrame(i_color) == 0) {
-                outputStream.notifyError(cvSink.getError());
-                continue;
-            }
+        while (!Thread.interrupted())
+            onLoop();
 
-            Imgproc.cvtColor(i_color, m_tmp, Imgproc.COLOR_BGR2HSV);
-            Core.inRange(m_tmp, orangeLow, orangeHigh, i_mask);
-            //Core.bitwise_not(i_mask, m_tmp);
-            //i_color.setTo(new Scalar(0, 0, 0), m_tmp);
+    }
 
-            pts.clear();
-            Imgproc.findContours(i_mask, pts, m_tmp, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-            pts.removeIf(sizeFilter);
-            pts.sort(sizeSorter);
-
-            if (pts.size() > 0) {
-                Rect bound = Imgproc.boundingRect(pts.get(0));
-                Imgproc.rectangle(i_color, bound, drawColor);
-                double line_y = bound.y + bound.height / 2;
-                double line_x = bound.x + bound.width / 2;
-                Point note_ctr = new Point(line_x, line_y);
-                Point screen_ctr = new Point(this.w / 2, line_y);
-                Imgproc.line(i_color, note_ctr, screen_ctr, drawColor, 3);
-
-                RotatedRect ell = Imgproc.fitEllipseDirect(i_mask);
-                //Imgproc.fitEllipse(null)
-                Point[] verts = new Point[4];
-                ell.points(verts);
-                for (int i = 0; i < 4; i++)
-                    Imgproc.line(i_color, verts[i], verts[(i+1)%4], drawColor);
-
-                // TODO report off-centeredness somehow, maybe to NT
-            }
-
-            outputStream.putFrame(i_color);
+    @Override
+    public void onLoop() {
+        if (this.cvSink.grabFrame(i_color) == 0) {
+            outputStream.notifyError(cvSink.getError());
+            return;
         }
+
+        Imgproc.cvtColor(i_color, m_tmp, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(m_tmp, orangeLow, orangeHigh, i_mask);
+        // Core.bitwise_not(i_mask, m_tmp);
+        // i_color.setTo(new Scalar(0, 0, 0), m_tmp);
+
+        pts.clear();
+        Imgproc.findContours(i_mask, pts, m_tmp, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        pts.removeIf(sizeFilter);
+        pts.sort(sizeSorter);
+
+        if (pts.size() > 0) {
+            Rect bound = Imgproc.boundingRect(pts.get(0));
+            Imgproc.rectangle(i_color, bound, drawColor);
+            double line_y = bound.y + bound.height / 2;
+            double line_x = bound.x + bound.width / 2;
+            Point note_ctr = new Point(line_x, line_y);
+            Point screen_ctr = new Point(this.w / 2, line_y);
+            Imgproc.line(i_color, note_ctr, screen_ctr, drawColor, 3);
+
+            //RotatedRect ell = Imgproc.fitEllipseDirect(i_mask);
+            //Imgproc.fitEllipse(null)
+            //Point[] verts = new Point[4];
+            //ell.points(verts);
+            //for (int i = 0; i < 4; i++)
+            //    Imgproc.line(i_color, verts[i], verts[(i + 1) % 4], drawColor);
+
+            this.dx = (line_x / this.w) - 0.5;
+            this.y = line_y / this.h;
+            submitTelemetry();
+        }
+
+        outputStream.putFrame(i_color);
+    }
+
+    @Override
+    public void submitTelemetry() {
+        this.p_dx.set(this.dx);
+        this.p_y.set(this.y);
+    }
+
+    @Override
+    public void receiveOptions() {
+        // not used atm
     }
 }
