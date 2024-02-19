a040fc898d64c6833e2238a7237e2418beeb9e2f
Kate Reed <josephreed2600@gmail.com>
2024-02-18 17:47:07 -0800

    beeeeep

--- src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
+++ src/main/java/frc/robot/subsystems/NoteTargetingPipeline.java
@@ -10,7 +10,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
