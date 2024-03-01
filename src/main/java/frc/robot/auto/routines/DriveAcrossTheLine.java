41583cb9cc71b566522eb841c2a2cce48eb931f0
LionRobotics-2907 <frcteam2907@gmail.com>
2024-03-01 15:10:07 -0800

    killll with 4 l's

--- src/main/java/frc/robot/auto/routines/DriveAcrossTheLine.java
+++ src/main/java/frc/robot/auto/routines/DriveAcrossTheLine.java
@@ -1,10 +1,7 @@
 package frc.robot.auto.routines;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.auto.actions.DriveToward;
-import frc.robot.auto.actions.SampleAction;
-import frc.robot.auto.actions.templates.ParallelAction;
 import frc.robot.auto.routines.templates.Routine;
 import frc.robot.constants.game_elements.FieldElements;
 import frc.robot.constants.game_elements.StrategicStructures;
